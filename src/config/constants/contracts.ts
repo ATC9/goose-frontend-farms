7e3abb8d1fb1c2584e7af2a07538e86adad863b7
ATC9 <79402847+ATC9@users.noreply.github.com>
2021-03-09 02:48:25 -0500

    Update contracts.ts

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,10 +1,10 @@
 export default {
   cake: {
-    56: '0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
+    56: '0x7c5BefeDe8b361283DA678aCbDA70Ba2d96e2D44',
     97: '',
   },
   masterChef: {
-    56: '0xe70E9185F5ea7Ba3C5d63705784D8563017f2E57',
+    56: '0x425F3cC7C04F8B370e3907B53C966a1410C56817',
     97: '',
   },
   wbnb: {
